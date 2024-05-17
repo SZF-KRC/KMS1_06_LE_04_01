efcd34d187f67c18209c115ba5fe2219cedf94fd
Jaroslav Krc <jaroslav.krc@gmail.com>
2024-05-17 11:45:02 +0200

    Projektdateien hinzufügen.

---
+++ KMS1_06_LE_04_01/GeometrischeFigur.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KMS1_06_LE_04_01
+{
+    public class GeometrischeFigur
+    {
+        public string Color { get; set; }
+        public virtual void CalculateArea()
+        {
+            Console.WriteLine("Tu bude kod");
+        }
+
+        public virtual void CalculatePerimeter() 
+        {
+            Console.WriteLine("tu bude kod");
+        }
+    }
+}
---
+++ KMS1_06_LE_04_01/Kreis.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KMS1_06_LE_04_01
+{
+    internal class Kreis : GeometrischeFigur
+    {
+        public override void CalculateArea()
+        {
+            base.CalculateArea();
+        }
+
+        public override void CalculatePerimeter()
+        {
+            base.CalculatePerimeter();
+        }
+    }
+}
